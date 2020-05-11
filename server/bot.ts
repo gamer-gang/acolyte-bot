842616b45cc7ed4191ce6c2660d97e80551cf2ab
wiisportsresort <devdoge1@gmail.com>
2020-05-10 20:49:39 -0400

    postinstall script to create data file

--- server/bot.ts
+++ server/bot.ts
@@ -1,13 +1,21 @@
+import * as ch from 'chalk';
 import * as Discord from 'discord.js';
 import { guildPrefs } from '.';
-import { GuildPrefs } from './types';
 import commands from './commands';
+import { GuildPrefs } from './types';
+import { Timer } from './common/util';
 
 const defaultPrefs: GuildPrefs = {
   prefix: '%',
 };
 
+let previousDuration = 0;
+
 export default async function (msg: Discord.Message) {
+  const timerId = ch`{green Processing message ${msg.id} took}`;
+
+  const timer = new Timer();
+
   const guildId = msg.guild?.id;
 
   if (msg.author.bot) return; // do not respond to other bots
@@ -38,10 +46,23 @@ export default async function (msg: Discord.Message) {
 
   msg.channel.send(
     `Got command \`${cmd != '' ? cmd : '(none)'}\`, ` +
-    `arguments \`[${args.join(', ')}]\`, ` +
-    `and flags \`[${flags.join(', ')}]\``,
+      `arguments \`[${args.join(', ')}]\`, ` +
+      `and flags \`[${flags.join(', ')}]\``,
   );
 
-  if (Object.prototype.hasOwnProperty.call(commands, cmd)) commands[cmd]({ msg, args, prefix, flags });
+  if (Object.prototype.hasOwnProperty.call(commands, cmd))
+    await commands[cmd]({ msg, args, prefix, flags });
+  else if (cmd === 'duration')
+    msg.channel.send(`Previous command took ${previousDuration.toFixed(3)}ms.`);
   else msg.channel.send('No command executed.');
+
+  timer.end();
+
+  previousDuration = timer.duration as number;
+
+  console.log(
+    ch`{green Processing for message ${msg.id} (${cmd}, [${args.join(', ')}], [${flags.join(
+      ', ',
+    )}]) completed in ${timer.duration}ms.}`,
+  );
 }
